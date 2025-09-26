65e2fed4439a12bbfa6ad338a562681dba48c60e
GTSpray <69210426+GTSpray@users.noreply.github.com>
2025-09-26 20:01:20 +0200

    lint fix

--- src/gateway/GatewaySocket.ts
+++ src/gateway/GatewaySocket.ts
@@ -1,46 +1,48 @@
 import EventEmitter from "events";
 import { Connection } from "./Connection";
-import { discordapi } from '../utils/discordapi';
+import { discordapi } from "../utils/discordapi";
 import { APIGatewayBotInfo, Routes } from "discord.js";
 
 export class GatewaySocket extends EventEmitter {
-    token: string;
-    shards: number | null;
-    sockets: Map<number, Connection>;
-    lastReady: number;
-    url: any;
-	constructor(token: string, shards?: number) {
-		super();
-		this.token = token;
-		this.shards = shards ? shards : null;
-		this.sockets = new Map();
-		this.lastReady = 0;
-	}
+  token: string;
+  shards: number | null;
+  sockets: Map<number, Connection>;
+  lastReady: number;
+  url: any;
+  constructor(token: string, shards?: number) {
+    super();
+    this.token = token;
+    this.shards = shards ? shards : null;
+    this.sockets = new Map();
+    this.lastReady = 0;
+  }
 
-	async connect(start = 0, end?: number) {
-		const { url, shards } = await discordapi.get(Routes.gatewayBot()) as APIGatewayBotInfo
-		this.url = url;
-		if (isNaN(<number>this.shards)) {
-			this.shards = shards;
-		}
-		end = end || <number>this.shards;
-		for (let i = start; i < end; i++) {
-            const oldSocket = this.sockets.get(i);
-			if (oldSocket) {
-				await oldSocket.close();
-			}
+  async connect(start = 0, end?: number) {
+    const { url, shards } = (await discordapi.get(
+      Routes.gatewayBot(),
+    )) as APIGatewayBotInfo;
+    this.url = url;
+    if (isNaN(<number>this.shards)) {
+      this.shards = shards;
+    }
+    end = end || <number>this.shards;
+    for (let i = start; i < end; i++) {
+      const oldSocket = this.sockets.get(i);
+      if (oldSocket) {
+        await oldSocket.close();
+      }
 
-            const co = new Connection(this, i);
-			this.sockets.set(i, co);
-            const {timeReady} = await co.connect();
-			this.lastReady = timeReady;
-		}
-	}
+      const co = new Connection(this, i);
+      this.sockets.set(i, co);
+      const { timeReady } = await co.connect();
+      this.lastReady = timeReady;
+    }
+  }
 
-	send(data: object, shard = 0) {
-        const socket = this.sockets.get(shard)
-        if(socket) {
-            socket.send(data);
-        }
-	}
+  send(data: object, shard = 0) {
+    const socket = this.sockets.get(shard);
+    if (socket) {
+      socket.send(data);
+    }
+  }
 }
--- src/gateway/index.ts
+++ src/gateway/index.ts
@@ -1,10 +1,9 @@
-
 import { GatewaySocket } from "./GatewaySocket";
 
 const socket = new GatewaySocket(process.env.BOT_TOKEN);
 
 socket.connect(0);
 
-socket.on('MESSAGE_CREATE', (shard: any, data: { content: any; }) => {
+socket.on("MESSAGE_CREATE", (shard: any, data: { content: any }) => {
   console.log(shard, data.content);
 });
