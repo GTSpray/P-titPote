7fa88d16150382c7d645ae7be49017178fca76cd
GTSpray <69210426+GTSpray@users.noreply.github.com>
2026-06-02 16:26:21 +0200

    fix: add a migration to compensate the migration generator following the version upgrade

---
+++ src/migrations/Migration20260602142257.ts
@@ -0,0 +1,23 @@
+import { Migration } from "@mikro-orm/migrations";
+
+export class Migration20260602142257 extends Migration {
+  override up(): void | Promise<void> {
+    this.addSql(
+      `alter table \`message_aliased\` drop foreign key \`message_aliased_server_id_foreign\`;`,
+    );
+
+    this.addSql(
+      `alter table \`message_aliased\` add constraint \`message_aliased_server_id_foreign\` foreign key (\`server_id\`) references \`discord_guild\` (\`id\`);`,
+    );
+  }
+
+  override down(): void | Promise<void> {
+    this.addSql(
+      `alter table \`message_aliased\` drop foreign key \`message_aliased_server_id_foreign\`;`,
+    );
+
+    this.addSql(
+      `alter table \`message_aliased\` add constraint \`message_aliased_server_id_foreign\` foreign key (\`server_id\`) references \`discord_guild\` (\`id\`) on update cascade on delete restrict;`,
+    );
+  }
+}
